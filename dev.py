f3272ed8f880531ceb243accda54ee94176514bc
etienne86 <barbier.et@gmail.com>
2019-07-31 12:38:51 +0200

    minor changes

--- dev.py
+++
@@ -1,8 +0,0 @@
-import googlemaps
-from config import Config
-
-gmaps = googlemaps.Client(key=Config.GOOGLE_MAPS_API_KEY)
-# Geocoding an institution
-geocode_result = gmaps.geocode('tour eifffel')
-
-print(geocode_result)
