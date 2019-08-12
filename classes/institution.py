ba5328288f986137c0ba07b7f6790c7ffb185009
etienne86 <barbier.et@gmail.com>
2019-08-12 15:21:48 +0200

    add tests, methods and functions linked to API's

--- classes/institution.py
+++ classes/institution.py
@@ -3,12 +3,18 @@
 
 """This module mainly contains the 'Institution' class."""
 
-import googlemaps
+import re
 
-from oc_p07_grandpy.various.config import Config
+import googlemaps
+import requests
+
+from various.config import Config
 
 
 class NoResponseError(Exception):
+    pass
+
+class NotFoundError(Exception):
     pass
 
 
@@ -27,6 +33,7 @@ class Institution():
         """
         This method is responsible for getting the
         formatted address from the googlemaps module.
+        This method returns a 'str' value.
         """
         try:
             return self.get_geocode_response()['formatted_address']
@@ -37,7 +44,7 @@ class Institution():
         """
         This method is responsible for getting the
         geocode response from the googlemaps module.
-        This method returns a dict, or raise an exception.
+        This method returns a 'dict' value, or raise an exception.
         """
         gmaps = googlemaps.Client(key=Config.GOOGLE_MAPS_API_KEY)
         geocode_result = gmaps.geocode(self.entered_name)
@@ -50,6 +57,7 @@ class Institution():
         """
         This method is responsible for getting the
         latitude from the googlemaps module.
+        This method returns a 'float' value.
         """
         try:
             return self.get_geocode_response()['geometry']['location']['lat']
@@ -60,30 +68,152 @@ class Institution():
         """
         This method is responsible for getting the
         longitude from the googlemaps module.
+        This method returns a 'float' value.
         """
         try:
             return self.get_geocode_response()['geometry']['location']['lng']
         except NoResponseError:
             return 'not understood so not found'
 
-    def get_standard_name(self):
+    def get_name(self):
         """
-        This method is responsible for getting the standard name,
-        based on the name entered by the user.
+        This method is responsible for getting the 'real' name,
+        based on the name entered by the user (and possibly mistyped).
+        This method returns a 'str' value.
         """
-        result = ""
-        return result
+        try:
+            return self.get_place_response()['result']['name']
+        except NoResponseError:
+            return 'not understood so not found'
+
+    def get_place_id(self):
+        """
+        This method is responsible for getting the
+        place_id from the googlemaps module.
+        This method returns a 'str' value.
+        """
+        try:
+            return self.get_geocode_response()['place_id']
+        except NoResponseError:
+            return 'not understood so not found'
+
+    def get_place_response(self):
+        """
+        This method is responsible for getting the
+        place response from the googlemaps module.
+        This method returns a 'dict' value, or raise an exception.
+        """
+        # execute the HTTP request
+        place_response_http = requests.get(
+            "https://maps.googleapis.com/maps/api/place/details/json?"\
+                + "placeid=" + self.get_place_id()\
+                + "&fields=name&key=" + Config.GOOGLE_MAPS_API_KEY
+        )
+        place_response_dict = place_response_http.json() # type is dict
+        # analyze and treat the HTTP response
+        if (place_response_http.status_code == 200) \
+                and (place_response_dict["status"] == "OK"):
+            return place_response_dict
+        else:
+            raise NoResponseError
 
     def get_wiki_response(self):
         """
         This method is responsible for getting the
         response from the wikipedia API.
+        This method returns a 'dict' value, or raise an exception.
         """
+        # execute the HTTP request
+        wiki_response_http = requests.get(
+            "https://fr.wikipedia.org/w/api.php?"\
+                + "action=parse&prop=text&format=json"\
+                + "&page=" + self.get_name()
+        )
+        wiki_response_dict = wiki_response_http.json() # type is dict
+        # analyze and treat the HTTP response
+        if wiki_response_http.status_code == 200:
+            return wiki_response_dict
+        else:
+            raise NoResponseError
    
     def get_wiki_summary(self):
         """
-        This method is responsible for extracting a few sentences
-        from the wikipedia page dedicated to the institution.
+        This method is responsible for extracting only a few sentences
+        from the wikipedia page text (got by the 'get_wiki_text' method).
+        This method returns a 'str' value.
         """
-        result = ""
-        return result        
+        # identify the correct section, which is after the following flag
+        flag = "class=\"mw-headline\""
+        splitted_text = self.get_wiki_text(flag)
+        # some sections are exceptions to be ignored
+        exceptions = [
+            "(.*)<div class=\"geobox\"(.*)", 
+            "(.*)<div class=\"infobox\"(.*)"
+        ]
+        # loop on the sections
+        n = 1
+        returned = False
+        while n < len(splitted_text):
+            # do not match the focused string with the exceptions
+            if (re.match(exceptions[0], splitted_text[n]) is None)\
+                    and (re.match(exceptions[1], splitted_text[n]) is None):
+                # get the content between the firsts <p> and </p>
+                temp_string = splitted_text[n].split("<p>")[1] # after '<p>'
+                temp_string = temp_string.split("</p>")[0] # before '</p>'
+                # clean the text
+                temp_string = ignore_http_tags(temp_string)
+                # shorten the text, if necessary
+                result = shorten_text(temp_string)
+                # indicate that the method will return a 'str' value
+                returned = True
+                return result
+            else:
+                n += 1
+        if not returned:
+            raise NotFoundError
+
+    def get_wiki_text(self):
+        """
+        This method is responsible for extracting the text
+        from the wikipedia page dedicated to the institution.
+        This method returns a 'str' value.
+        """
+        try:
+            return self.get_wiki_response()['parse']['text']['*']
+        except NoResponseError:
+            return 'not understood so not found'
+
+
+def ignore_http_tags(string):
+    """
+    This function returns the given string, without the http tags.
+    """
+    result = ""
+    regex = r"<.*?>" # regular expression for an http tag
+    # keep only what is not http tags
+    result_list = re.split(regex, string)
+    # concatenate all elements
+    for sub in result_list:
+        result += sub
+    return result
+
+
+def shorten_text(string):
+    """
+    This function returns the given string, shortened to max 1000 characters.
+    """
+    result = string
+    if len(string) > 1000:
+        # cut to 1000 characters
+        result = string[:999]
+        # find the last index of "."
+        ind = result.rfind(".")
+        # cut properly
+        if ind != -1: # at least one occurence of "." is found
+            # cut at the end of a sentence
+            result = result[:ind + 1]
+        else: # no "." is found among 1000 first characters
+            space_ind = result.rfind(" ")
+            # cut at the end of a word
+            result = result[:space_ind] + "..."
+    return result
--- classes/tests/test_institution.py
+++ classes/tests/test_institution.py
@@ -8,16 +8,20 @@ from unittest.mock import Mock
 import pytest
 import googlemaps
 
-import oc_p07_grandpy.classes.institution as institution
-from institution import Institution
+from oc_p07_grandpy.classes.institution import Institution
+from oc_p07_grandpy.classes.institution import ignore_http_tags, shorten_text
 
 
-# class TestInstitution():
-
+# initialization of a function which mocks:
+# - two googlemaps returns:
+#   _ 'geocode' return for address, latitude, longitude and place_id
+#   _ 'place' return for real name
+# - the wikipedia return to get the page text
 mock_func = Mock()
 
 def setup_function():
-    # a function which mocks the googlemaps 'geocode' return
+    # the mock function returns a dict
+    # (with the same keys as in the 'geocode' and 'place' returns)
     mock_func.return_value = {
         "formatted_address": "mock_address",
         "geometry": {
@@ -25,27 +29,76 @@ def setup_function():
                 "lat": "mock_latitude",
                 "lng": "mock_longitude"
             }
+        },
+        "place_id": "mock_place_id",
+        "result": {
+            "name" : "mock_name"
+        },
+        "parse": {
+            "text": {
+                "*": "mock_wiki_text"
+            }
         }
     }
-    
-def test_get_formatted_address(monkeypatch):
+
+def init_for_geocode_tests(monkeypatch):
+    """
+    This function is used in several tests
+    related to the googlemaps 'geoccode' return.
+    """
     # the mocking process with the 'monkeypatch' fixture
     monkeypatch.setattr(Institution, 'get_geocode_response', mock_func)
     new_inst = Institution("")
-    value = new_inst.get_formatted_address()
+    return new_inst
+
+def test_get_formatted_address(monkeypatch):
+    value = init_for_geocode_tests(monkeypatch).get_formatted_address()
     assert value == "mock_address"
 
-
 def test_get_latitude(monkeypatch):
-    # the mocking process with the 'monkeypatch' fixture
-    monkeypatch.setattr(Institution, 'get_geocode_response', mock_func)
-    new_inst = Institution("")
-    value = new_inst.get_latitude()
+    value = init_for_geocode_tests(monkeypatch).get_latitude()
     assert value == "mock_latitude"
 
 def test_get_longitude(monkeypatch):
-    # the mocking process with the 'monkeypatch' fixture
-    monkeypatch.setattr(Institution, 'get_geocode_response', mock_func)
-    new_inst = Institution("")
-    value = new_inst.get_longitude()
+    value = init_for_geocode_tests(monkeypatch).get_longitude()
     assert value == "mock_longitude"
+
+def test_get_name(monkeypatch):
+    monkeypatch.setattr(Institution, 'get_place_response', mock_func)
+    new_inst = Institution("")
+    value = new_inst.get_name()
+    assert value == "mock_name"
+
+def test_get_place_id(monkeypatch):
+    value = init_for_geocode_tests(monkeypatch).get_place_id()
+    assert value == "mock_place_id"
+
+def test_get_wiki_summary():
+    new_inst = Institution("")
+    full_text = new_inst.get_wiki_text()
+
+def test_get_wiki_text(monkeypatch):
+    monkeypatch.setattr(Institution, 'get_wiki_response', mock_func)
+    new_inst = Institution("")
+    value = new_inst.get_wiki_text()
+    assert value == "mock_wiki_text"
+
+
+def test_ignore_http_tags():
+    string = "<strong>Hello!</strong> We are <em>glad</em> to see you!"
+    simple_string = "Hello! We are glad to see you!"
+    assert ignore_http_tags(string) == simple_string
+
+def test_shorten_text_with_short_string():
+    string = "12345678. " * 50 + "123"
+    assert shorten_text(string) == string
+
+def test_shorten_text_with_long_string():
+    string = "12345678. " * 110 + "123"
+    shortened_string = "12345678. " * 99 + "12345678."
+    assert shorten_text(string) == shortened_string
+
+def test_shorten_text_without_any_dot():
+    string = "123456789 " * 110 + "123"
+    shortened_string = "123456789 " * 98 + "123456789" + "..." 
+    assert shorten_text(string) == shortened_string
