daf377bedd8a8ce54b00ca6f23ca0bce46810b67
etienne86 <barbier.et@gmail.com>
2019-08-20 08:20:14 +0200

    add user_question tests and functionalities

--- classes/tests/test_user_question.py
+++ classes/tests/test_user_question.py
@@ -1,43 +1,122 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-"""This module contains the 'UserQuestion' class."""
+"""This module contains the tests related to the 'UserQuestion' class."""
 
-class UserQuestion():
-    """docstring for UserQuestion"""
-    
-    def __init__(self, entered_question):
-        """This special method is the class constructor."""
-        # super(UserQuestion, self).__init__()
-        self.entered_question = entered_question
+from unittest.mock import Mock
 
-    def get_splitted(self):
-        """
-        This method is responsible for splitting
-        the entered question into words.
-        """
-        ## TO DO
-        # check if self.entered_question type is str
-        # split()
-        pass
+import pytest
 
-    def parse(self):
-        """
-        This method is responsible for parsing the entered question
-        into key words, if the question seems appropriate.
-        """
-        ## TO DO:
-        result = ""
-        ## split sentence into words
-        ## ignore stop words
-        ## algo
-        ## result = " ".join(key_words)
-        return result
+from oc_p07_grandpy.classes.institution import Institution
+from oc_p07_grandpy.classes.user_question import UserQuestion
 
-    def request_location(self):
-        """
-        This method is responsible for determining if the question seems
-        appropriate. This returns 'True' if yes, 'False' if not.
-        """
-        ## TO DO
-        pass
+
+# initialization of three functions which mock three googlemaps returns:
+# - a list of 0 result
+mock_func_0 = Mock()
+mock_func_0.return_value = []
+# - a list of 1 result
+mock_func_1 = Mock()
+mock_func_1.return_value = [{}]
+# - a list of several results (e.g. 2 results)
+mock_func_2 = Mock()
+mock_func_2.return_value = [{}, {}]
+
+# nota: analyze() is not tested directly,
+# as it uses methods already tested
+
+def test_ask_complete_question_true():
+    """
+    The user asks a complete question,
+    with at least one key word to determine an address to search.
+    """
+    sentence = "Comment va-t-on à la poste ?"
+    question = UserQuestion(sentence)
+    assert question.ask_complete_question() == True
+
+def test_ask_complete_question_false():
+    """
+    The user does not ask a complete question,
+    because there are not key words enough in the sentence.
+    # It assumes that the user question has been already parsed.
+    """
+    sentence = "Comment vas-tu ?"
+    question = UserQuestion(sentence)
+    assert question.ask_complete_question() == False 
+
+def test_ask_for_location_true_with_one_key_word():
+    """The user asks really for an address with one key word."""
+    sentence = "Où est la poste ?"
+    question = UserQuestion(sentence)
+    assert question.ask_for_location() == True
+
+def test_ask_for_location_true_with_one_phrase():
+    """The user asks really for an address with one phrase (several words)."""
+    sentence = "Pourrais-tu me dire comment aller à la poste s'il te plaît ?"
+    question = UserQuestion(sentence)
+    assert question.ask_for_location() == True
+
+def test_ask_for_location_false():
+    """The user does not ask for an address."""
+    sentence = "Au petit déjeuner, j'aime boire du thé."
+    question = UserQuestion(sentence)
+    assert question.ask_for_location() == False
+
+def test_ask_precise_question_false_zero_result(monkeypatch):
+    """The user does not precise the institution clearly,
+    and Google Maps returns 0 result."""
+    monkeypatch.setattr(Institution, 'get_geocode_response', mock_func_0)
+    question = UserQuestion("")
+    assert question.ask_precise_question() == False
+
+def test_ask_precise_question_true(monkeypatch):
+    """The user precises the institution clearly,
+    and Google Maps returns 1 result."""
+    monkeypatch.setattr(Institution, 'get_geocode_response', mock_func_1)
+    question = UserQuestion("")
+    assert question.ask_precise_question() == True
+
+def test_ask_precise_question_false_several_results(monkeypatch):
+    """The user does not precise the institution clearly,
+    and Google Maps returns several results."""
+    monkeypatch.setattr(Institution, 'get_geocode_response', mock_func_2)
+    question = UserQuestion("")
+    assert question.ask_precise_question() == False
+
+def test_parse():
+    """This function is responsible for testing
+    the parsing method on several sentences.
+    """
+    sentences = [
+        "",
+        "Comment vas-tu ?",
+        "Où est la poste ?",
+        "Pourrais-tu me dire comment aller à la poste s'il te plaît ?",
+        "Hey GrandPy, salut ! Comment vas-tu ? J'aimerais savoir " +\
+            "où se trouve la Tour Montparnasse s'il te plaît ?",
+        "Je veux aller faire un tour au zoo de Beauval. Il paraît que " +\
+            "cet endroit est merveilleux.",
+        "Coucou mon vieux, peux-tu me dire où se trouve " +\
+            "la poste de Poitiers STP ?",
+        "salut grand py bot, est ce que tu connais " +\
+            "la basilique de saint maximin",
+        "ou est la tour eiffel",
+        "ou trouver la cathedrale notre dame"
+    ]
+    parsed_sentences = [
+        "",
+        "",
+        "Poste",
+        "Poste",
+        "Tour Montparnasse",
+        "Zoo Beauval",
+        "Poste Poitiers",
+        "Basilique Saint Maximin",
+        "Tour Eiffel",
+        "Cathedrale Dame"
+    ]
+    results = []
+    for _ in range(len(sentences)):
+        question = UserQuestion(sentences.pop(0))
+        results.append(question.parse() == parsed_sentences.pop(0))
+    return not (False in results)
--- classes/user_question.py
+++ classes/user_question.py
@@ -3,6 +3,12 @@
 
 """This module contains the 'UserQuestion' class."""
 
+import re
+
+# from oc_p07_grandpy.classes.bot_reply import BotReply
+from oc_p07_grandpy.classes.institution import Institution, NoResponseError
+import oc_p07_grandpy.classes.stop_words as stop_words
+
 
 class UserQuestion():
     """This class is used to get and treat the question asked by the user."""
@@ -12,36 +18,110 @@ class UserQuestion():
         super(UserQuestion, self).__init__()
         self.entered_question = entered_question # type is str
 
-    def get_splitted(self):
+    def analyze(self, bot):
         """
-        This method is responsible for splitting
-        the entered question into words.
+        This method is responsible for analyzing the user question,
+        and calls other methods.
+        This returns a dict to feed the bot reply.
         """
-        ## TO DO
-        # check if self.entered_question type is str
-        # split()
-        pass
+        # is is a question to request an address?
+        if not self.ask_for_location:
+            bot.ask_for_understanding()
+            self.analyze(bot)
+        # is the question complete enough?
+        if not self.ask_complete_question():
+            bot.ask_for_completion()
+            self.analyze(bot)
+        # is the question precise enough?
+        if not self.ask_precise_question():
+            bot.ask_for_precision()
+            self.analyze(bot)
+
+    def ask_complete_question(self):
+        """
+        This method is responsible for determining
+        if the question is complete enough.
+        This returns 'True' if yes, 'False' if not.
+        """
+        if self.parse() is "":
+            return False
+        else:
+            return True
+
+    def ask_for_location(self):
+        """
+        This method is responsible for determining
+        if the question seems appropriate to ask for an address.
+        This returns 'True' if yes, 'False' if not.
+        """
+        key_words = [
+            "adresse", "comment all", "comment va", "ou", "où"
+        ]
+        result = False
+        while key_words and not result:
+            if key_words.pop(0) in self.entered_question.lower():
+                result = True
+        return result                    
+
+    def ask_precise_question(self):
+        """
+        This method is responsible for determining
+        if the question is precise enough.
+        The question is considered as precise if there is one,
+        and only one result returned by Google Maps.
+        This returns 'True' if yes, 'False' if not.
+        """
+        inst = Institution(self.parse())
+        try:
+            length = len(inst.get_geocode_response())
+            if length == 1:
+                return True
+            else:
+                return False
+        except NoResponseError:
+            return False
 
     def parse(self):
         """
         This method is responsible for parsing the entered question
         into key words, if the question seems appropriate.
         """
-        ## TO DO:
-        result = ""
-        ## split sentence into words
         # remove punctuation
+        string = re.sub(r"([,\?;\.\:!/\\\*\(\)\[\]])*",
+                        "",
+                        self.entered_question)
         # remove redundant spaces
+        string = re.sub(r"( ){2,}", " ", string)
         # transform to lowercase
-        ## exclude stop words
-        ## algo
-        ## result = " ".join(key_words)
-        return result
+        string = string.lower()
+        # split
+        words_list = string.split()
+        # exclude stop words
+        limited_list = [word for word in words_list \
+            if word not in stop_words.all_fr]
+        # exclude stop expressions (two consecutive words)
+        lower_result = exclude_expressions(limited_list)
+        # capitalize for Google Maps and Wikipedia search
+        capitalized_result = [word.capitalize() for word in lower_result]
+        return " ".join(capitalized_result)
 
-    def request_location(self):
-        """
-        This method is responsible for determining if the question seems
-        appropriate. This returns 'True' if yes, 'False' if not.
-        """
-        ## TO DO
-        pass
+# sub function used in the method parse()
+def exclude_expressions(words):
+    """
+    This function excludes some expressions from parsing.
+    """
+    result = words # type is list
+    consec = stop_words.consecutive_fr
+    for i in range(len(words) - 1):
+        for j in range(len(consec)):
+            if words[i] == consec[j][0] and words[i+1] == consec[j][1]:
+                # remove the two consecutive words
+                if i == 0 and i+1 == len(words) - 1: # if list is only two words
+                    result = []
+                elif i == 0: # if words are at the beginning
+                    result = words[i+2:] # keep only the end
+                elif i+1 == len(words) - 1: # if words are at the end
+                    result = result[:i]
+                else:
+                    result = result[:i] + words[i+2:]
+    return result
