c1f94b834941c6ddc1638e3a4474fe558e1268b3
etienne86 <barbier.et@gmail.com>
2019-08-27 09:22:13 +0200

    start main program

--- grandpybot.py
+++ grandpybot.py
@@ -12,18 +12,18 @@ def main():
     # say hello to the user
     bot_hello = BotReply()
     bot_hello.welcome_message()
-    # loop as far as the user asks an acceptable question
-    cont = True
-    while cont:
+    # initialize the interactive bot
+    bot = BotReply()
+    # ask for the user question
+    entered_question = input("votre question svp ? ")
+    user_quest = UserQuestion(entered_question)
+    # loop as far as the user does not ask an acceptable question
+    while not user_quest.analyze(bot)['acceptable_question']:
         # ask for the user question
         entered_question = input("votre question svp ? ")
         user_quest = UserQuestion(entered_question)
-        # initialize the interactive bot
-        bot = BotReply()
-        # initialize the institution
-        inst = Institution(user_quest.analyze(bot)['question'])
-        # stop or continue?
-        cont = not user_quest.analyze(bot)['acceptable_question']
+    # initialize the institution
+    inst = Institution(user_quest.analyze(bot)['question'])
     # display the results
     print(bot.give_answer_first(inst))
     print(bot.give_answer_second(inst))
