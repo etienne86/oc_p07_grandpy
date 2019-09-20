849f7d7a2e8fafbc9eb1282839f9f3c1e6f8f3ee
etienne86 <barbier.et@gmail.com>
2019-09-20 16:14:10 +0200

    add interaction with python in html/css/js files

--- app/static/js/grandpy.js
+++ app/static/js/grandpy.js
@@ -1,24 +1,116 @@
+///////////////////////////////////////////////////////////////////////////////
+//                        WELCOME MESSAGE FROM THE BOT                       //
+///////////////////////////////////////////////////////////////////////////////
+
+var divElt = document.createElement("div");
+divElt.setAttribute("class", "bot_reply");
+var welcome = document.getElementById("js_01").getAttribute("wlcmMsg");
+divElt.textContent = welcome;
+document.getElementById("dialogue").appendChild(divElt);
 
 
-var pElt = document.createElement("p");
-pElt.setAttribute("class", "bot_reply");
+///////////////////////////////////////////////////////////////////////////////
+//         MANAGE THE EVENT 'SUBMIT' WHEN CLICKING ON BUTTON 'Envoyer'       //
+///////////////////////////////////////////////////////////////////////////////
+
+var map_number = 0;
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // add the user question into the dialogue (i.e. display the user question)
+    var question = form.elements.user_input.value;
+    var divElt = document.createElement("div");
+    divElt.setAttribute("class", "user_question");
+    divElt.textContent = question;
+    document.getElementById("dialogue").appendChild(divElt);
+    // empty the textarea (inside the form)
+    var formElt = document.getElementById("form");
+    formElt.reset();
+    // send data to the server
+    $.post( "/postmethod", {
+        entered_data: JSON.stringify(question)
+    }, function (err, req, resp) {
+        var phrases = resp["responseJSON"]["phrases"];
+        // display either the address (if the question was understood)
+        // or the reason why the bot did not understand the question
+        divElt = document.createElement("div");
+        divElt.setAttribute("class", "bot_reply");
+        // divElt.textContent = phrases;
+        divElt.textContent = phrases[0];
+        document.getElementById("dialogue").appendChild(divElt);
+        // if the question was understood by the bot
+        if (phrases.length !== 1) {
+            // display the map
+            var divElt = document.createElement("div");
+            divElt.setAttribute("class", "map");
+            document.getElementById("dialogue").appendChild(divElt);
+            var myMap = resp["responseJSON"]["map"];
+            var myLat = myMap.lat;
+            var myLng = myMap.lng;
+            var myTitle = myMap.title;
+            var myZoom = myMap.zoom;
+            displayMap(myLat, myLng, myZoom, myTitle);
+            map_number = map_number + 1;
+            // display the short story (from wikipedia)
+            divElt = document.createElement("div");
+            divElt.setAttribute("class", "bot_reply");
+            divElt.textContent = phrases[1];
+            document.getElementById("dialogue").appendChild(divElt);
+        }
+    });
+});
+
+function displayMap(myLat, myLng, myZoom, myTitle) {
+    var myLatLng = {lat: myLat, lng: myLng};
+
+    var map = new google.maps.Map(document.getElementsByClassName('map')[map_number], {
+        zoom: myZoom,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: myTitle
+    });
+}
+
+// function getData(question) {
+//     $.post( "/postmethod", {
+//         entered_data: JSON.stringify(question)
+//     });
+// }
 
 
-titreLien.textContent = {{ bot.welcome_message() }};
-titreLien.href = unLien.url;
-titreLien.setAttribute("style", "color: #428bca; font-weight: bold; text-decoration: none;");
-pElt.appendChild(titreLien);
 
-var spanElt = document.createElement("span");
-spanElt.style.color = "black";
-spanElt.style.fontWeight = "normal";
 
-var brElt = document.createElement("br");
+///////////////////////////////////////////////////////////////////////////////
+//                              DISPLAY THE MAP                              //
+///////////////////////////////////////////////////////////////////////////////
 
-spanElt.appendChild(document.createTextNode(" " + unLien.url));
-spanElt.appendChild(brElt);
-spanElt.appendChild(document.createTextNode("Ajouté par " + unLien.auteur));
-pElt.appendChild(spanElt);
+// var divElt = document.createElement("div");
+// divElt.setAttribute("class", "map");
+// document.getElementById("dialogue").appendChild(divElt);
 
-document.getElementById("contenu").appendChild(pElt);
+// // passing the python variables to the map
+// var myLat = Number(document.getElementById("js_01").getAttribute("lat"));
+// var myLng = Number(document.getElementById("js_01").getAttribute("lng"));
+// var myZoom = Number(document.getElementById("js_01").getAttribute("zoom"));
+// var myTitle = document.getElementById("js_01").getAttribute("markerTitle");
+
+// function initMap() {
+//     var myLatLng = {lat: myLat, lng: myLng};
+
+//     var map = new google.maps.Map(document.getElementsByClassName('map')[0], {
+//         zoom: myZoom,
+//         center: myLatLng
+//     });
+
+//     var marker = new google.maps.Marker({
+//         position: myLatLng,
+//         map: map,
+//         title: myTitle
+//     });
+// }
 
