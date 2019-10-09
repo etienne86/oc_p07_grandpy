7eb86c7c517f3b36a746c9faed1abce65a789751
etienne86 <barbier.et@gmail.com>
2019-10-09 11:58:14 +0200

    adapt config.py to have all tests green

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
 class Config(object):
-    GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
+    if os.environ.get("GOOGLE_MAPS_API_KEY") is not None:
+        GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
+    else:
+        GOOGLE_MAPS_API_KEY = ""
